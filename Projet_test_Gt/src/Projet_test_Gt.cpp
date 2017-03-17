9ff6f0decc477e0f497aba3c5efdfd258ae45ab3
mrasamimanananivo <miaina.rasamimanananivo@insa-rouen.fr>
2017-03-17 11:52:37 +0100

    ajout ligne affichage

---
+++ Projet_test_Gt/src/Projet_test_Gt.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Projet_test_Gt.cpp
+// Author      : Cynthia Rasamimanananivo
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
